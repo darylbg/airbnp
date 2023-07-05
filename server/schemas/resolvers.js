0d1fb219c665f28862252b8c4cc8a948da939364
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-07-05 10:52:25 +0100

    resolvers.js file edited

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -60,7 +60,7 @@ const resolvers = {
         if (context.user) {
           const updatedUser = await User.findOneAndUpdate(
             { _id: context.user._id },
-            { $pull: { savedToilets: { _id: { toiletIdId } } },
+            { $pull: { savedToilets:   { toiletId } } },
             { new: true }
           );
   
