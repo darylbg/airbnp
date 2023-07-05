7ad55785522e57501c7c01069cf737c6c55a33b3
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-07-05 10:29:03 +0100

    Toilet.js initial version

--- server/models/Toilet.js
+++ server/models/Toilet.js
@@ -2,11 +2,21 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedToilets` array in User.js
 const toiletSchema = new Schema({
-  authors: [
+    name: [
     {
       type: String,
+      required: true
     },
   ],
+  lat : {
+    type: Number,
+    required: true,
+  },
+  lng : {
+    type: Number,
+    required: true,
+  },
+
   description: {
     type: String,
     required: true,
