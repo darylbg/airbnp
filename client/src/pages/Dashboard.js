3e615aaaab5a979687f375a34daf5265b7f288ac
Quas <aaron.hickman201@gmail.com>
2023-07-09 20:38:39 +0100

    my listings

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import ListingsForm from '../components/NewListing/NewListing'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import NewListing from '../components/NewListing/NewListing';
+import LocalListings from '../components/LocalListings/LocalListings';
+import MyListings from '../components/MyListings/MyListings';
 
 const Dashboard = () => {
   return (
-    <div className='container' style={{width: '100%', justifyContent: 'center', display: 'flex' }}>
-
-        <ListingsForm />
-    </div>
+   <Container className='d-flex justify-content-center'>
+    <Row>
+      <Col xs={4} sm={4}>
+        <NewListing />
+      </Col>
+      <Col xs={4} sm={4}>
+        <MyListings />
+      </Col>
+    </Row>
+   </Container>
   )
 }
 
